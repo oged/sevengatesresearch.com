27ff1300cc4551fafff18d7d6d3f371c66efaabf
oged <80210586+oged@users.noreply.github.com>
2026-09-06 11:16:12 +0300

    Serve final Republic hero image

--- app/images/research/republic-of-30-percent-nigeria-ponzi-schemes/hero-final.png/route.ts
+++ app/images/research/republic-of-30-percent-nigeria-ponzi-schemes/hero-final.png/route.ts
@@ -5,15 +5,11 @@ import part3 from './part3';
 import part4 from './part4';
 import part5 from './part5';
 import part6 from './part6';
-import part7 from './part7';
-import part8 from './part8';
-import part9 from './part9';
-import part10 from './part10';
 
 export const runtime = 'nodejs';
 export const dynamic = 'force-static';
 
-const IMAGE_BASE64 = [part0, part1, part2, part3, part4, part5, part6, part7, part8, part9, part10].join('');
+const IMAGE_BASE64 = [part0, part1, part2, part3, part4, part5, part6].join('');
 const IMAGE_BYTES = Buffer.from(IMAGE_BASE64, 'base64');
 
 export async function GET() {
